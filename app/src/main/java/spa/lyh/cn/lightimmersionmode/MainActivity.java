e1aff3cc1618339876c31e5fb9dc0d68ee25fa27
liyuhao <liyuhaoid@sina.com>
2017-04-25 22:53:08 +0800

    先创建一个库

---
+++ app/src/main/java/spa/lyh/cn/lightimmersionmode/MainActivity.java
@@ -0,0 +1,13 @@
+package spa.lyh.cn.lightimmersionmode;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
